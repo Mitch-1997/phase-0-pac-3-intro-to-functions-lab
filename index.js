a814eaea1f3f730cb45aa108a6bc05af59b7407f
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-08-14 23:35:42 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShoutString() {
+    console.log(string.toUpperCase());
+}
+function logWhisperString() {
+    console.log(string.toLowerCase());
+}
+function sayHiToHeadPhoneRoommate(string) {
+    if (string.toLowerCase() === string) {
+        return "I can\'t hear you!";
+    } 
+}
+if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+}
+if (string === "Let's have dinner together!") {
+    return "I would love to!";
+}
+function sayHiToHeadPhoneRommate(string) {
+    if (string.toLowerCase() === string)
+    return "I would love too";
+}
